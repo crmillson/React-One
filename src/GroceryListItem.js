2702d6d4da76c632c73bcf35e566bc8537672c2b
Kwisatz-Haderachs <frcranmer@gmail.com>
2022-10-11 16:15:07 -0500

    hooks

--- src/GroceryListItem.js
+++ src/GroceryListItem.js
@@ -1,33 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default class GroceryListItem extends React.Component{
-    constructor(props) {
-        super(props);
+export default function GroceryListItem({item}) {
+    const[bold, setBold] = useState(false);
+    const[done, setDone] = useState(false);
 
-        this.state = {
-            done: false,
-            bold: false
-        };
-    }
-
-    onListItemClick(){
-        this.setState({done: !this.state.done});
-    }
-
-
-    render() {
-        return (
-            <li style={{textDecoration: this.state.done ? 'line-through' : 'none',
-                    fontWeight: this.state.bold ? 'bold' : 'normal',}}
-                onMouseEnter={() => {
-                    this.setState({bold:true})
-                }}
-                onMouseLeave={() => {
-                    this.setState({bold:false})
-                }}
-                onClick={this.onListItemClick.bind(this)} >
-                {this.props.item.name} : {this.props.item.quantity}</li>
-        )
-    }
+    return (
+        <li style={{textDecoration: done ? 'line-through' : 'none',
+                fontWeight: bold ? 'bold' : 'normal',}}
+            onMouseEnter={() => {
+                setBold(true)
+            }}
+            onMouseLeave={() => {
+                setBold(false)
+            }}
+            onClick={() => setDone(true)} >
+            {item.name} : {item.quantity}</li>
+    )
 }
 
